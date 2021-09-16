35a585a6253e95e0c33902f948daf56636e1de2b
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-09-16 07:58:05 +0200

    Enonce 1, question 7

--- Program.cs
+++ Program.cs
@@ -89,6 +89,7 @@ namespace Cours_C_
                 */
 
                 // ******************************* Enoncé 1, question 6 ******************************
+                /*
                 Console.WriteLine("Entrez une valeur de type int");
                 int a = int.Parse(Console.ReadLine());
                 Console.WriteLine("Le type et la taille de la variable a est : ");
@@ -104,9 +105,58 @@ namespace Cours_C_
                 Console.WriteLine("Le type et la taille de la variable Result est : ");
                 Console.WriteLine(Result.GetType());  
 
-
+                */
 
                 // ****************************** Enoncé 1, question 7 **************************************************
+                
+                Console.WriteLine("");
+                Console.WriteLine("Conversion d'un double vers un short");
+                Console.WriteLine("");
+
+                Console.WriteLine("Entrez un nombre flotant");
+                double nb_float = double.Parse(Console.ReadLine());
+                short nb_short = (short)nb_float;
+
+                Console.WriteLine("Le nombre flotant de type double que vous avez entré {0}, converti en type short donne {1}", nb_float, nb_short);
+
+
+                Console.WriteLine("");
+                Console.WriteLine("Conversion d'un int vers un sbyte");
+                Console.WriteLine("");
+
+                Console.WriteLine("Entrez un nombre entier");
+                int nb_int = int.Parse(Console.ReadLine());
+                sbyte nb_sbyte = (sbyte)nb_int;
+
+                if (nb_int > 127 || nb_int < -128)
+                {   
+                    Console.WriteLine("Le nombre entier de type int que vous avez entré {0}, converti en type sbyte donne {1}", nb_int, nb_sbyte);
+                    Console.WriteLine("La valeur du sbyte est différente du int car un sbyte ne peut qu'avoir une valeur comprise entre -128 et 127");
+                }
+                else
+                {
+                    Console.WriteLine("Le nombre entier de type int que vous avez entré {0}, converti en type sbyte donne {1}", nb_int, nb_sbyte);
+                }
+
+
+                Console.WriteLine("");
+                Console.WriteLine("Conversion d'un int vers un ushort");
+                Console.WriteLine("");
+
+                Console.WriteLine("Entrez un nombre entier");
+                int nb_int2 = int.Parse(Console.ReadLine());
+                ushort nb_ushort = (ushort)nb_int2;
+
+                if (nb_int2 < 0 || nb_int2 > 65535)
+                {   
+                    Console.WriteLine("Le nombre entier de type short que vous avez entré {0}, converti en type ushort donne {1}", nb_int2, nb_ushort);
+                    Console.WriteLine("La valeur du short est différente du ushort car un ushort ne peut qu'avoir une valeur comprise entre 0 et 65535");
+                }
+                else
+                {
+                    Console.WriteLine("Le nombre entier de type int que vous avez entré {0}, converti en type sbyte donne {1}", nb_int2, nb_ushort);
+                }
+
 
             }
         
