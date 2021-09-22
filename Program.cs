71bf594d3534372a72ae8c6235fa9ac0c38dba85
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-09-22 22:35:39 +0200

    TP1_22.09

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Cours_C_
 {
@@ -216,29 +217,271 @@ namespace Cours_C_
     //     }
     // }
 
-    // ******************************* Comparer deux valeurs ************************************
+    // ******************************* Comparer de valeurs grâce aux méthodes Replace, Insert & Substring ************************************
 
-    String s = "bonjour";
-    Console.WriteLine($"La taille de la chaine est de {s.Length} caractères");
+    // String s = "bonjour";
+    // Console.WriteLine($"La taille de la chaine est de {s.Length} caractères");
 
-    s=s.Replace("O","Z");
-    Console.WriteLine("La chaine devient : " +s);
+    // s=s.Replace("O","Z");
+    // Console.WriteLine("La chaine devient : " +s);
 
-    s=s.Insert(3,"-y-");
-    Console.WriteLine("La chaine devient : " +s);
-    if (s.Contains("ur")==true)
-    Console.WriteLine("La chaine contient ur");
+    // s=s.Insert(3,"-y-");
+    // Console.WriteLine("La chaine devient : " +s);
+    // if (s.Contains("ur")==true)
+    // Console.WriteLine("La chaine contient ur");
     
-    if (s.Equals("Bonjour")==false)
-    Console.WriteLine("La chaine n'est pas égale à bonjour");
+    // if (s.Equals("Bonjour")==false)
+    // Console.WriteLine("La chaine n'est pas égale à bonjour");
 
-    String m =s.Substring(0,2);
-    Console.WriteLine("Les deux premières lettres sont : " +m);
-    Console.WriteLine("La chaine en majuscule : " +s.ToUpper());
-    Console.WriteLine("La chaine en majuscule : " +s.ToLower());
+    // String m =s.Substring(0,2);
+    // Console.WriteLine("Les deux premières lettres sont : " +m);
+    // Console.WriteLine("La chaine en majuscule : " +s.ToUpper());
+    // Console.WriteLine("La chaine en majuscule : " +s.ToLower());
 
-}
+
+
+// ****************************** Enoncé 2 :  Traitement de chaînes de caractères  **************************************** //
+
+
+        // 1. Afficher les deux chaînes concaténées en la chaîne S.
+
+            // String S1 = "John";
+            // String S2 = "Doe";
+
+            // String s = S1 + "" +S2;
+            // Console.WriteLine(a);
+
+            // }
+
+            // 2. Peut-on concaténer un entier N à S ?
+
+            string userName = "John";
+            string date = DateTime.Today.ToShortDateString();
+
+            // Use string interpolation to concatenate strings.
+            string str = $"Hello {userName}. Today is {date}.";
+            System.Console.WriteLine(str);
+
+            // string Name = "John";
+            // int a = 15;
+
+            // Console.WriteLine(a + "" + Name);
+
+            // 3. Afficher la longueur de cette chaîne S.
+
+            //  Console.WriteLine($"La taille de la chaine est de {s.Length} caractères");
+
+            //  4. Afficher S en Majuscules puis en Minuscules.
+
+            // Console.WriteLine("La chaine en majuscule : " +s.ToUpper());
+            // Console.WriteLine("La chaine en majuscule : " +s.ToLower());
+
+            // 5. Rechercher un caractère ou une sous chaîne spécifique dans S avec la 
+            // méthode IndexOf en affichant le résultat.
+
+            // Console.WriteLine(s.IndexOf("o"));  // Affiche la positon d'une lettre
+
+            //          6. Extraire une sous chaîne de X caractères à partir de la position Y de S 
+            //          avec la méthode Substring puis l’afficher
+
+            // String x =s.Substring(0,2);
+            // Console.WriteLine("Les deux premières lettres sont : " +x);
+
+            //          7. Tester le code suivant et analyser les différentes comparaisons faites 
+            //          entre deux chaînes :
+
+            // String S1="Hello";
+            // string S2="Hello!";
+
+            // int N=string.Compare(S1,S2);                                // Compare deux objets String spécifiés et retourne un entier qui indique leur position relative dans l'ordre de tri.
+            // Console.WriteLine("Résultat avec Compare :"+N);  
+
+            // N=string.CompareOrdinal(S1,S2);
+            // Console.WriteLine("Résultat avec CompareOrdinal :"+N);      // Compare deux objets String spécifiés en évaluant les valeurs numériques des objets Char correspondants dans chaque chaîne.
+
+            // N=S1.CompareTo(S2);
+            // Console.WriteLine("1ier Résultat avec CompareTo : "+N);      // Compare cette instance avec un Object spécifié et indique si cette instance précède, suit ou apparaît à la même position dans l'ordre de tri que le Object spécifié.
+
+            // 9. Peut-on modifier une chaîne de caractères ? Quel est l’inconvénient ?
+
+            //             char[] Tab=new char[]{'C','h','a','i','n','e'};
+            //             string S=string.Empty;
+            //             Console.WriteLine("Chaine S :"+S);
+
+            //             foreach(var car in Tab)
+            //             { S=S+car; // ou S+=car;
+            //             Console.WriteLine("Chaine S :"+S);
+            //             }
+
+
+            //             // Version StringBuilder permet de modifier une chaine de caractère
+            //             StringBuilder Sb=new StringBuilder();
+            //             Console.WriteLine("Chaine Sb :"+Sb); 
+
+            //             foreach(var car in Tab)
+            //             { Sb.Append(car); // ou S+=car;b                      
+            //             Console.WriteLine("Chaine Sb :"+Sb);
+            //             }
+
+            //             Sb.Insert(0,"Ma_");
+            //             Console.WriteLine(Sb);
+
+            //             Sb.Remove(2,1);
+            //             Console.WriteLine(Sb);
+
+            //             Sb.Replace('M', 'T');
+            //             Console.WriteLine(Sb);
+
+            //             Sb.AppendLine();
+            //             Sb.Append("_Test");
+            //             Console.WriteLine(Sb);
+
+            // }
+
+
+            // ****************************** Enoncé 3 :  Traitement de tableaux  **************************************** //
+
+
+
+            // 1. Donner la différence entre un tableau statique et un tableau dynamique
+
+            // Les tableaux statiques reçoivent de la mémoire au moment de la compilation et la mémoire est allouée sur la pile. 
+            // Alors que les tableaux dynamiques se voient allouer de la mémoire au moment de l'exécution et que la mémoire est allouée à partir du tas.
+
+
+            // 2. Définir un tableau d’entiers dynamique une dimension puis le trier selon l’ordre croissant.
+
+            // int[] x = new int[]   { 1, 2 ,3} ;
+            // Array.Sort(x); // Fonctionne sort ordonne de manière croissante le code
+
+            // foreach(int Value in x)
+            // {
+            // Console.WriteLine(Value);  
+            // }
+
+            // Console.WriteLine("Element at index [0, 0] : " +x [0,0]);
+
+            // }
+
+            // 3. Définir un tableau deux dimensions d’entiers dynamique puis le trier.
+
+
+
+
+
+            // ****************************** Enoncé 4 :  Testde conditionnelles/Switch/Boucles  **************************************** //
+
+
+
+            // 1. Afficher chaque caractère de votre nom dans une boucle
+
+            // Console.WriteLine("Saisir votre nom : ");
+            // string Votre_Nom=Console.ReadLine();
+
+            // foreach (char c in Votre_Nom)
+            // {
+            //     Console.WriteLine(c);
+            // }
+
+            // }
+            // }
+
+            // 2. Afficher les entiers compris entre 1 et 10 sans le 8.
+
+            // for (int x = 0; x <= 10; x++) {
+            //     if (x==8) 
+            //     continue;
+            //     else Console.WriteLine(); 
+            // }
+
+
+            // 3. Afficher les entiers de 0 à 15 par pas de 2.
+
+            // for(int x=0; x<=15;x++){
+            //     if(x%2 = 0) continue;
+            //     else Console.WriteLine(x);
+            // }
+
+
+
+            // 4. Dans une boucle inviter l’utilisateur à saisir un prix HT (0 pour Terminer) et afficher sa valeur TTC.
+
+            // string String_TVA;
+            // Console.WriteLine("Saisir votre prix HT avec , 0 pour finir :");
+            // float PHT=float.Parse(Console.ReadLine());
+
+            // while (PHT > 0) {
+            //     Console.WriteLine("Saisir le taux de TVA sans le % : ");
+
+            //     String_TVA=Console.ReadLine();
+            //     float TVA=float.Parse(String_TVA);
+
+            //     float PTTC=PHT*(1+TVA/100);
+            //     Console.WriteLine("Prix TTC : " +PTTC);
+            //     Console.WriteLine("Saisir votre prix HT si vous voulez continuer sinon 0");
+            //     PHT=float.Parse(Console.ReadLine());
+
+            // }
+
+            // 5. Ecrire un programme qui affiche la suite de symboles suivants :
+            // * 
+            // ** 
+            // *** 
+            // **** 
+            // ***** 
+            // ****** 
+            // *******
+
+// ****************************** Solution A **********************************    
+
+            // StringBuilder etoile = new StringBuilder();
+            // for (int v=1;v<6; v++) {
+            //     etoile.Append("*");                         // Drop l'élément etoile
+            //     Console.WriteLine(etoile.ToString());
+            // }
+
+
+// ****************************** Solution B **********************************  
+
+    //     int val = 5;  
+    //     int i, j, k ;  
+
+    //     for (i = 1; i <= val; i++)  
+    //     {  
+    //         for (j = 1; j <= i; j++)  {  
+    //            Console.Write("");  
+    //         }  
+    //         for (k = 1; k <= i; k++)  {  
+    //            Console.Write("*");  
+    //         }  
+    //         Console.WriteLine("");  
+    //      }  
+    //      Console.ReadLine();  
+    //   }
+
+// ****************************** Solution C ********************************** 
+
+            int b, c = 0;
+
+            Console.WriteLine("Saisir une valeur");
+            int a = int.Parse(Console.ReadLine());  
+
+            for(b = 0; b < a;){
+            Console.Write("*");
+            c++;
+                if (c == (a-b)){
+                Console.WriteLine(""); 
+                b++;
+                c = 0;
+                } 
+            }
+            Console.ReadLine();  
 
     }
+            
 }
+
+}
+
+
 
