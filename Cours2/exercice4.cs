e18a4047fac937e53cccc5059b2925fba40be2dc
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-09-22 18:49:35 +0200

    Rangement

---
+++ Cours2/exercice4.cs
@@ -0,0 +1,62 @@
+// using System;
+// using System.Text;
+
+// namespace Cours_C_
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Ecrire votre nom");
+//             string nom = Console.ReadLine();
+
+//             foreach (char c in nom)
+//             {
+//                 Console.WriteLine(c);
+//             }
+
+
+//             for (int i = 1; i <= 10; i++)
+//             {
+//                 if (i == 8)
+//                 {
+//                     continue;
+//                 }
+//                 Console.WriteLine(i);
+//             }   
+
+//             for (int i = 0; i <= 15; i+=2)
+//             {
+//                 if (i == 8)
+//                 {
+//                     continue;
+//                 }
+//                 Console.WriteLine(i);
+//             } 
+
+            
+
+            
+//             string string_TVA;
+//             Console.WriteLine("Saisir le prix HT avec ,0 pour finir : ");
+//             float PHT= float.Parse(Console.ReadLine());
+
+//             while (PHT > 0)
+//             {
+//                 Console.WriteLine("Ajouter TVA sans %");
+//                  string_TVA = Console.ReadLine();
+//                  float TVA = float.Parse(string_TVA);
+
+//                  float PTTC = PHT*(1+TVA/100);
+//                  Console.WriteLine("Prix TTC :" +PTTC);
+//                  Console.WriteLine("Saisir votre prix HT si vous voulez continuer, sinon 0");
+                 
+//                  Console.WriteLine("Saisir le prix HT avec ,0 pour finir : ");
+//                  PHT=float.Parse(Console.ReadLine());
+//             }
+            
+            
+//         }
+        
+//     }
+// }
