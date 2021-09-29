4f551bb7af050a2630077f6d96918404554e134d
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-09-29 12:11:29 +0200

    Update exercice3.cs

--- Cours2/exercice3.cs
+++ Cours2/exercice3.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 // // using System;
 // // using System.Text;
 
@@ -9,18 +8,6 @@
 // //         static void Main(string[] args)
 // //         {
 // //             // int[] tableau = new int[] {2, 8, 7, 54, 10, 0};
-=======
-// using System;
-// using System.Text;
-
-// namespace Cours_C_
-// {
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             // int[] tableau = new int[] {2, 8, 7, 54, 10, 0};
->>>>>>> Stashed changes
             
 //             // // Array.Sort(tableau);
 //             // int save;
