eecce64c45ed83629333b48c5878ec377440bc76
Theo Meunier <theo.tatsuki@gmail.com>
2021-09-29 15:24:58 +0200

    Methodes
    Methodes c#

---
+++ Méthode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Méthode
+{
+    class Program
+    {
+       static double Convertion(int a){
+
+           return a * 1.17;
+       }
+
+        static void Main(string[] args)
+        {
+             int[] Euro = {1,2,3,500,1200};
+             double[] Dollar = new double[Euro.Length];
+             int indenf;
+             
+             for(int i = 0;i < Euro.Length; i++){
+                 
+                 Dollar[i] = Convertion(Euro[i]);
+                 Console.WriteLine($"{Euro[i]} Euro est égale à {Dollar[i]} dollar.");
+             }
+
+        }
+    }
+}
