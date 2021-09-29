31f2c176ed224bb1d3a0b4263f1aaf84fe1cfe6a
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-09-29 12:10:52 +0200

    ...

--- Program.cs
+++ Program.cs
@@ -7,11 +7,7 @@ namespace Cours_C_
     {
         static void Main(string[] args)
         {
-<<<<<<< Updated upstream
                 
-=======
-            
->>>>>>> Stashed changes
         }
         
     }
