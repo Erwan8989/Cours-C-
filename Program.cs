69eb9585a8b1b7478ec58d6f94b31c069a6f99e2
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-09-29 12:49:16 +0200

    ...

--- Program.cs
+++ Program.cs
@@ -4,20 +4,10 @@ using System.Text;
 namespace Cours_C_
 {
     class Program
-    {
-       private static void Affiche(out string nom){ // ajouter out pour appel sans valeur initialisée
-        nom="Titi";
-       }
-        
-
-        static void Main(string[] args)
+    { 
+         static void Main(string[] args)
         {
-            // Appel sans initialisation
-        string Nombis = "test"; // coucou paramètre réel ou effectif
-        //Console.WriteLine("Paramètre réel : "+Nombis);
-        Affiche(out Nombis); // paramètre formel/réel modifiés, même paramètre
-        Console.WriteLine("Paramètre réel : "+Nombis); // paramètre réel non modifié
-
+           
         }
         
     }
