3ab0544397c245ebae3d17b3dccd5513d4000a07
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-10-03 20:19:02 +0200

    Conf tu

---
+++ Tests_Unitaires/Sources/Lib/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Lib
+{
+    public class Class1
+    {
+        // Fonctions simples
+        public static int Somme(int a, int b)
+        {
+        return a + b;
+        }
+        public static int Produit(int a, int b)
+        {
+        return a * b;
+        }
+    }
+}
