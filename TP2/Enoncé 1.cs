5d1c98df0143a09498f5e483f3b3dafff9987b35
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-10-06 12:11:01 +0200

    Update

--- TP2/Enoncé 1.cs
+++ TP2/Enoncé 1.cs
@@ -1,91 +1,102 @@
-﻿using System;
+﻿// using System;
 
-namespace TP2
-{
-    class program 
-    {
+// namespace TP2
+// {
+//     class program 
+//     {
 
-        static void Main(string[] args)
-        {
-            console.WriteLine ("Helloworld");
+//         static void Main(string[] args)
+//         {
+//             console.WriteLine ("Helloworld");
 
-        // Déclaration et création des objets de classe Ressources
+//         // Déclaration et création des objets de classe Ressources
                   
-        } 
-    }
+//         } 
+//     }
 
     
-    // Déclaration de la classe Service 
-    class Services {
-
-        private int nbRessourcesLibres;
-        private int nbRessourcesOccupées;
-
-        // Déclaration du ou des constructeurs
-
-        public service (){
-
-        }
-
-        public service (int nbrl) {
-            this.nbRessourcesLibres=nbrl;
-            this.nbRessourcesOccupées=0;
-        }
-
-        public service (int nbrl, int nbro) {
-            this.nbRessourcesLibres=nmrl;
-            this.nbRessourcesOccupées=nbro;
-        }
-
-        // getters pour la lecture et accès aux attributs en private :fonctions.
-
-        public int get_nbRessourcesLibres() {
-            return nbRessourcesLibres;
-        }
-
-        public int get_nbRessourcesOccupées() {
-            return nbRessourcesOccupées;
-        }
-
-        // Setters pour modifier les attributs en private 
-
-        public void set_nbRessourcesLibres(int nb) { // nb > 0
-            this.nbRessourcesLibres=nb;
-        }
-
-        public void set_nbRessourcesOccupées(int nb) { // nb > 0
-            this.nbRessourcesOccupées=nb;
-        }
-
-
-        // Déclaration des méthodes diverses
-
-        public void PriseRessources (int nb) {
-            if (get_nbRessourcesLibres() >=nb) {
-                nbRessourcesLibres =- nb;
-                nbRessourcesOccupées =+ nb;
-            }
-
-            else {
-                console.WriteLine("Plus de ressources disponibles...");
-            }
+//     // Déclaration de la classe Service 
+//     class Services {
         
-        }
+//         // Déclaration des attributs ou bien des caractéristiques des objets
+//         // En privés, sécurisés avec accès via get et set
+//         private int nbRessourcesLibres;
+//         private int nbRessourcesOccupées;
 
-        // Doit passer par une méthode public pour faire appel à une méthode privée
+//         // Déclaration du ou des constructeurs
 
-        public void RendreRessources (int nb) {
-            if (get_nbRessourcesOccupées () >= nb) {
-                nbRessourcesLibres =+ nb;
-                nbRessourcesOccupées =- nb;
-            }
-            else {
-                console.WriteLine("Plus de ressources occupées...");
-            }
+//         public service (){
+//         }
 
-        }
+//         // Constructeurs pas paramètres
+//         public service (int nbrl) {
+//             this.nbRessourcesLibres=nbrl;
+//             this.nbRessourcesOccupées=0;
+//         }
 
-        }
-    }
+//         public service (int nbrl, int nbro) {
+//             this.nbRessourcesLibres=nmrl;
+//             this.nbRessourcesOccupées=nbro;
+//         }
+
+//         // getters pour la lecture et accès aux attributs en private :fonctions.
+
+//         public int get_nbRessourcesLibres() {
+//             return nbRessourcesLibres;
+//         }
+
+//         public int get_nbRessourcesOccupées() {
+//             return nbRessourcesOccupées;
+//         }
+
+//         // Setters pour modifier les attributs en private 
+
+//         public void set_nbRessourcesLibres(int nb) { // nb > 0
+//             this.nbRessourcesLibres=nb;
+//         }
+
+//         public void set_nbRessourcesOccupées(int nb) { // nb > 0
+//             this.nbRessourcesOccupées=nb;
+//         }
+
+
+//         // Déclaration des méthodes diverses
+
+//         public void PriseRessources (int nb) {
+//             if (get_nbRessourcesLibres() >=nb) {
+//                 nbRessourcesLibres =- nb;
+//                 nbRessourcesOccupées =+ nb;
+//             }
+
+//             else {
+//                 console.WriteLine("Plus de ressources disponibles...");
+//             }
+        
+//         }
+
+//         // Doit passer par une méthode public pour faire appel à une méthode privée
+
+//         public void RendreRessources (int nb) {
+//             if (get_nbRessourcesOccupées () >= nb) {
+//                 nbRessourcesLibres =+ nb;
+//                 nbRessourcesOccupées =- nb;
+//             }
+//             else {
+//                 console.WriteLine("Plus de ressources occupées...");
+//             }
+
+           /* // public int RessourcesLibres () {
+            //     return nbRessourcesLibres;
+            // } */
+
+//             public void afficher_Ressources () {
+//                 console.WriteLine("Nombre de ressources libres " + get_nbRessourcesLibres());
+//                 console.WriteLine("njombre de ressources occupées " + get_nbRessourcesOccupées());
+//             }
+
+//         }
+
+//         }
+//     }
 
     
--- TP2/Enoncé 2.cs
+++ TP2/Enoncé 2.cs
@@ -5,46 +5,47 @@ using System;
 
         class Etudiant
         {
-            private int note;
-            private string nom;
-            private string prenom;
+            private int Note;
+            private string Nom;
+            private string Prenom;
 
             public string Student() // Constructeur vide
             {}
 
-            public string Student(int age, string nom, string prenom)
+            public void Student(int note, string nom, string prenom)
             {
-                age = Age; 
-                nom = Nom; 
-                prenom = Prenom;
+                this.Note = note;
+                this.Nom = nom;
+                this.Prenom = prenom;
+                          
             }
   
             // Création des getters
 
-            public int get_age (int age) {
-                return age;
+            public int get_note () {
+                return Note;
             }
 
-            public int get_nom (string nom) {
-                return nom;
+            public int get_nom () {
+                return Nom;
             }
 
-            public int get_prenom (string prenom) {
-                return prenom;
+            public int get_prenom () {
+                return Prenom;
             }
 
             // Définiton des Setters
 
-            public void set_age (int a) {
-                this.age=a;
+            public void set_note (int a) {
+                this.Note=a;
             }
 
             public void set_prenom (string p) {
-                this.prenom=p;
+                this.Prenom=p;
             }
 
             public void set_nom (string n) {
-                this.nom=n;
+                this.Nom=n;
             }
 
         }
@@ -54,9 +55,16 @@ using System;
             public static void Main()
             {
 
-                Etudiant set_age = new Etudiant(age = 123);
-                Etudiant set_prenom = new Etudiant(prenom = "Fluffy");
-                Etudiant set_nom = new Etudiant(nom  = "Michel");
+                Etudiant eleve1 = new Etudiant(25, "Doe", "John" );
+
+                Console.WriteLine(eleve1.get_prenom() + " " + eleve1.get_nom() + " " + eleve1.get_note());
+
+
+                // Etudiant set_note = new Etudiant(Note = 123);
+                // Etudiant set_prenom = new Etudiant(Prenom = "Fluffy");
+                // Etudiant set_nom = new Etudiant(Nom  = "Michel");
+
+
             }
 
         }           
