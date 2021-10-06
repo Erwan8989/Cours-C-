45098c76921517df8264641f42cf945507951cd5
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-10-06 12:32:54 +0200

    Ajout de commentaires

--- TP Noté Exercice 2/Program.cs
+++ TP Noté Exercice 2/Program.cs
@@ -6,34 +6,35 @@ namespace TP_Noté_Exercice_2
     {
         static void Main(string[] args)
         {
-            Etudiant eleve1 = new Etudiant("John", "Doe", 18);
+            Etudiant eleve1 = new Etudiant("John", "Doe", 18); // instanciation de la classe Etudiant, en passant en paramètre le nom, prénom et la note
 
-            Console.WriteLine("L'élève " + eleve1.getPrenom() + " " + eleve1.getNom() + " a eu " + eleve1.getNote() + " sur 20");
+            Console.WriteLine("L'élève " + eleve1.getPrenom() + " " + eleve1.getNom() + " a eu " + eleve1.getNote() + " sur 20"); // Appel de méthodes de la classe Etudiant pour afficher le nom, prenom et la note
             
         }
     }
 
     class Etudiant
-    {
+    {   
+        // Création des attributs de la classe
         private string nomEtudiant;
         private string prenomEtudiant;
         private int noteEtudiant;
 
-        public Etudiant(string prenom, string nom, int note){
+        public Etudiant(string prenom, string nom, int note){ // Constructeur
             this.prenomEtudiant = prenom;
             this.nomEtudiant = nom;
             this.noteEtudiant = note;
         }
 
-        public string getNom(){
+        public string getNom(){ // création de la méthode getNom pour afficher le nom
             return this.nomEtudiant;
         }
 
-        public string getPrenom(){
+        public string getPrenom(){ // création de la méthode getPrenom pour afficher le prenom
             return this.prenomEtudiant;
         }
 
-        public int getNote(){
+        public int getNote(){ // création de la méthode getNote pour afficher la note
             return this.noteEtudiant;
         }
     }
