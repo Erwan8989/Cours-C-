8b3d640a1e4aeeda6a3cd82d458865e4d0822daf
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2021-10-06 10:04:12 +0200

    TP - Test Simple (fonctionnel)

--- TP Noté Exercice 2/Program.cs
+++ TP Noté Exercice 2/Program.cs
@@ -6,7 +6,35 @@ namespace TP_Noté_Exercice_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Etudiant eleve1 = new Etudiant("John", "Doe", 18);
+
+            Console.WriteLine("L'élève " + eleve1.getPrenom() + " " + eleve1.getNom() + " a eu " + eleve1.getNote() + " sur 20");
+            
+        }
+    }
+
+    class Etudiant
+    {
+        private string nomEtudiant;
+        private string prenomEtudiant;
+        private int noteEtudiant;
+
+        public Etudiant(string prenom, string nom, int note){
+            this.prenomEtudiant = prenom;
+            this.nomEtudiant = nom;
+            this.noteEtudiant = note;
+        }
+
+        public string getNom(){
+            return this.nomEtudiant;
+        }
+
+        public string getPrenom(){
+            return this.prenomEtudiant;
+        }
+
+        public int getNote(){
+            return this.noteEtudiant;
         }
     }
 }
