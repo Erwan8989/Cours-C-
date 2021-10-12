f7481406a028960548462efb5ed088c0dcb71b51
Noodles-boop <Oldoni.jeremy@outlook.fr>
2021-10-12 22:10:42 +0200

    ut

--- Tests_Unitaires/Sources/Lib/Class1.cs
+++ Tests_Unitaires/Sources/Lib/Class1.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 namespace Lib
 {
-    public class Class1
-    {
-        // Fonctions simples
-        public static int Somme(int a, int b)
-        {
-        return a + b;
-        }
-        public static int Produit(int a, int b)
-        {
-        return a * b;
-        }
+    // public class Class1
+    // {
+    //     // Fonctions simples
+    //     public static int Somme(int a, int b)
+    //     {
+    //     return a + b;
+    //     }
+    //     public static int Produit(int a, int b)
+    //     {
+    //     return a * b;
+    //     }
+    // }
+public class Calcules {
+    public double CalculerMoyenne(double Math, double francais, double physique) {
+        double moyenne;
+
+        int c1 = 5, c2 = 7, c3 = 3;
+        int total = c1 + c2 + c3;
+
+        moyenne = (Math * c1 + physique * c2 + francais * c3) / total;
+
+        return moyenne;
+
     }
+}
 }
--- Tests_Unitaires/test/UnitTest1.cs
+++ Tests_Unitaires/test/UnitTest1.cs
@@ -6,17 +6,22 @@ namespace test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
-        {
-        int SOM = Class1.Somme(2, 3); 
-        Xunit.Assert.Equal(5, SOM);
+        // public void Test1()
+        // {
+        // int SOM = Class1.Somme(2, 3); 
+        // Xunit.Assert.Equal(5, SOM);
+        // }
+        // [Fact]
+        // public void Test2()
+        // {
+        // int PROD = Class1.Produit(4,5);
+        // Xunit.Assert.Equal(20, PROD);
+        // }
+
+        public void testMethod () {
+            Calcules c = new Calcules();
+            double m = c.CalculerMoyenne(11,11,12);
+            Xunit.Assert.Equal(m, 11.2);
         }
-        [Fact]
-        public void Test2()
-        {
-        int PROD = Class1.Produit(4,5);
-        Xunit.Assert.Equal(20, PROD);
-        }
- 
     }
 }
