fc115aa8980bbfb431960827b548eb429ae00c52
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2021-10-19 14:30:38 +0200

    ...

--- Tests_Unitaires_Bis/RessourcesLibTest/UnitTest1.cs
+++ Tests_Unitaires_Bis/RessourcesLibTest/UnitTest1.cs
@@ -1,20 +1,20 @@
-using System;
-using Xunit;
-using RessourcesLib;
+// using System;
+// using Xunit;
+// using RessourcesLib;
 
-namespace RessourcesLibTest
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
-            Services Salle1=new Services(15);
-            Salle1.Afficher_Ressources();
-            Xunit.Assert.Equal(15, Salle1.get_NbRessourceslibres());
+// namespace RessourcesLibTest
+// {
+//     public class UnitTest1
+//     {
+//         [Fact]
+//         public void Test1()
+//         {
+//             Services Salle1=new Services(15);
+//             Salle1.Afficher_Ressources();
+//             Xunit.Assert.Equal(15, Salle1.get_NbRessourceslibres());
 
-        }
+//         }
      
-        }
-    }
+//         }
+//     }
 
