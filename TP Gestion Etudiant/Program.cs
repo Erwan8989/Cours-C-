8087799470d74efb86c9934afbe8565c67455963
Noodles-boop <Oldoni.jeremy@outlook.fr>
2021-11-09 12:45:36 +0100

    ...

---
+++ TP Gestion Etudiant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP_Gestion_Etudiant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
