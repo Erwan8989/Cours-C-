e673a051e98adbe89e668997d6aa97681bfccd7a
Noodles-boop <Oldoni.jeremy@outlook.fr>
2021-11-11 10:36:16 +0100

    ...

--- TP3/Program.cs
+++ TP3/Program.cs
@@ -57,7 +57,6 @@ namespace Compte_Bancaire // espace de nom
 // Ajout d'une saisie utilisateur 
 // Utilisation du mot clé new pour le masquage du membre hérité 
         public void Affichage(){
-
             Console.WriteLine("Titulaire : {0};\nN° de compte: {1};\nMontant :   {2};\n", this.Titulaire , this.NumeroCompte , this.Solde);
         }
          
@@ -71,24 +70,18 @@ namespace Compte_Bancaire // espace de nom
                 Console.Write("Combien voulez-vous deposer sur votre compte : ");
                 montant = int.Parse(Console.ReadLine());
 
-                if (montant <= 0)
-                {
-                    Console.WriteLine("Crédit impossible");
+                if (montant >= 0) {
+
+                    this.Solde += montant;
+                    Console.WriteLine("Votre solde est maintenant de : " + this.Solde);  
                 }
 
-            } 
-
-            this.Solde += montant;
-            Console.WriteLine("Votre solde est maintenant de : " + this.Solde); 
-        
-        }
-
-        public void depot(double montant) {
-            Crediter(montant);
-        }
-
-        public void repos(double prixRetrait) {
-            Debiter(prixRetrait);
+                else 
+                {
+                    Console.WriteLine("Crédit impossible");
+                    break;
+                }
+            }                 
         }
 
         protected void Debiter(double prixRetrait){
@@ -101,16 +94,17 @@ namespace Compte_Bancaire // espace de nom
                 Console.Write("qu'elle montant voulez vous retirer : ");
                 prixRetrait = int.Parse(Console.ReadLine());
 
-                if (this.Solde < prixRetrait)
+                if ((this.Solde - prixRetrait) >= 0)
                 {
+                    this.Solde -= prixRetrait;
+                    Console.WriteLine("Votre solde est maintenant de : " + this.Solde);         
+                } 
+
+                else {
                     Console.WriteLine("Solde insuffisant !");
                     break;
                 }
-
-            } 
-
-            this.Solde -= prixRetrait;
-            Console.WriteLine("Votre solde est maintenant de : " + this.Solde);
+            }          
         }
 
         public void Taxe()
@@ -120,7 +114,7 @@ namespace Compte_Bancaire // espace de nom
             this.Solde -= 5;  
             this.date ++;
             if (this.date == 12){
-                this.date = 1;
+                this.date = 1;  
             }}       
         }
 
@@ -134,6 +128,13 @@ namespace Compte_Bancaire // espace de nom
                 this.date2 = 1;
                 }
             }       
+        }
+         public void depot(double montant) {
+            Crediter(montant);
+        }
+
+        public void retrait(double prixRetrait) {
+            Debiter(prixRetrait);
         }
 } 
 // fin classe mère
@@ -156,7 +157,7 @@ namespace Compte_Bancaire // espace de nom
 
                 else
                 {
-                    Console.WriteLine("Pas de fond suffisant pour profiter des interets!\n");
+                    Console.WriteLine("Pas de fond suffisant pour profiter des intérets!\n");
                 }
         }
     }
@@ -181,7 +182,7 @@ namespace Compte_Bancaire // espace de nom
 
             else
             {
-                Console.WriteLine("Ce compte ne peut etre taxe!\n");
+                Console.WriteLine("Ce compte ne peut etre taxé !\n");
             }
         }
     }
@@ -194,15 +195,19 @@ namespace Compte_Bancaire // espace de nom
             // Scenario 1 :
             Console.WriteLine("Test Scenario Compte Bancaire :\n");    
 
-            Compte CB1 = new Compte("OULMI Céline",2339483,0,0.05);
+            Compte CB1 = new Compte("Michel OBAMA",2339483,20000,0.11);
             CB1.Affichage();
             CB1.depot(0);
             CB1.Affichage();
-            CB1.repos(0);
+            CB1.retrait(0);
+            CB1.Affichage();
+            CB1.Taxe();
             CB1.Affichage();
             CB1.depot(0);
             CB1.Affichage();
-            CB1.repos(0);    
+            CB1.TauxInteret();
+            CB1.Affichage();
+            CB1.retrait(0);    
 
             // Scenario 2 :
             Console.WriteLine("Test Scenario Compte Cheque :\n");
@@ -213,7 +218,7 @@ namespace Compte_Bancaire // espace de nom
             CC1.Affichage();
             CC1.depot(0);
             CC1.Affichage();
-            CC1.repos(0);
+            CC1.retrait(0);
             CC1.Taxe();
             CC1.Affichage();
             CC1.TauxInteret();
@@ -224,27 +229,25 @@ namespace Compte_Bancaire // espace de nom
 
             // Scenario 3 :
 
-        //     Console.WriteLine("Test Scenario Compte Livret :\n");
+            Console.WriteLine("Test Scenario Compte Livret :\n");
 
-        //     CompteLivret CL1 = new CompteLivret("John Danone",547362,0,0.1);
-        //     CL1.Affichage();
-        //     CL1.depot(0);
-        //     CL1.Affichage();
-        //     CL1.depot(0);
-        //     CL1.Affichage();
-        //     CL1.repos(0);
-        //     CL1.Taxe();
-        //     CL1.TauxInteret();
-        //     CL1.Affichage();
-        //     CL1.depot(0);
-        //     CL1.Taxe();
-        //     CL1.Affichage();
-        // }
+            CompteLivret CL1 = new CompteLivret("John DOE",547362,0,0.1);
+            CL1.Affichage();
+            CL1.depot(0);
+            CL1.Affichage();
+            CL1.retrait(0);
+            CL1.Taxe();
+            CL1.TauxInteret();
+            CL1.Affichage();
+            CL1.depot(0);
+            CL1.Taxe();
+            CL1.Affichage();
+        }
     }
          
 }
 
-}
+
 
   
 
